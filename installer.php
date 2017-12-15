6be4faec7cfe9dd8bb41b8d61f8abe0cc42fa057
Brandon Wilson <wilso199@miamioh.edu>
2017-12-15 16:28:28 -0500

    Adding files to create a template app for hackathon students to build from.

---
+++ installer.php
@@ -0,0 +1,30 @@
+<?php
+
+	/*
+	* Copyright (C) 2016-2017 Abre.io LLC
+	*
+	* This program is free software: you can redistribute it and/or modify
+    * it under the terms of the Affero General Public License version 3
+    * as published by the Free Software Foundation.
+	*
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU Affero General Public License for more details.
+	*
+    * You should have received a copy of the Affero General Public License
+    * version 3 along with this program.  If not, see https://www.gnu.org/licenses/agpl-3.0.en.html.
+    */
+
+	//Required configuration files
+	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
+	require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
+
+	if(superadmin() && !file_exists("$portal_path_root/modules/Abre-Starter/setup.txt")){
+
+		//Write the Setup File
+		// $myfile = fopen("$portal_path_root/modules/Abre-Conduct/setup.txt", "w");
+		// fwrite($myfile, '');
+		// fclose($myfile);
+	}
+?>
---
+++ permissions.php
@@ -0,0 +1,32 @@
+<?php
+
+	/*
+	* Copyright (C) 2016-2017 Abre.io LLC
+	*
+	* This program is free software: you can redistribute it and/or modify
+    * it under the terms of the Affero General Public License version 3
+    * as published by the Free Software Foundation.
+	*
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU Affero General Public License for more details.
+	*
+    * You should have received a copy of the Affero General Public License
+    * version 3 along with this program.  If not, see https://www.gnu.org/licenses/agpl-3.0.en.html.
+    */
+
+	//Required configuration files
+	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
+	require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
+	require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
+
+	//Check for Admin Authentication
+	$drawerhidden = 1;
+	$pagerestrictions = "staff, students, parent";
+
+	if($_SESSION['usertype'] == 'staff'){
+		$drawerhidden=0;
+		$pagerestrictions="";
+	}
+?>
